7632b37ccf7e8f4ed729ebb739b667c5a42b0941
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-01-15 09:18:10 +0900

    week1

--- src/main/java/com/springdingdong/hany/SpringDingDongApplication.java
+++ src/main/java/com/springdingdong/hany/SpringDingDongApplication.java
@@ -1,6 +1,8 @@
 package com.springdingdong.hany;
 
 
+import com.springdingdong.hany.assignment1.Stock;
+import com.springdingdong.hany.assignment1.StockInterface;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,11 @@ public class SpringDingDongApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDingDongApplication.class, args);
+        Stock stock = new Stock();
+
+        stock.PrintStockInfo("005390","프디아우라");
+        stock.PrintCountry();
+        System.out.println(StockInterface.COUNTRY);
 
     }
 
